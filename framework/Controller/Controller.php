1fae68f6c36ea7bc29b624042b851e3fe1ae06d0
Lyhoshva Vlad <lyhoshvavlad@gmail.com>
2016-03-05 23:38:54 +0200

    Added some methods to Controller

--- framework/Controller/Controller.php
+++ framework/Controller/Controller.php
@@ -2,8 +2,25 @@
 
 namespace Framework\Controller;
 
+use Framework\DI\Service;
+use Framework\Response\ResponseRedirect;
 
 class Controller
 {
+    public function redirect($redirect_to, $message = '')
+    {
+        //@TODO Add message implementation
+        return new ResponseRedirect($redirect_to);
+    }
 
+    public function generateRoute($route)
+    {
+        $router = Service::get('router');
+        return $router->generateRoute($route);
+    }
+
+    public function getRequest()
+    {
+        return Service::get('request');
+    }
 }
--- framework/DI/Service.php
+++ framework/DI/Service.php
@@ -2,15 +2,34 @@
 
 namespace Framework\DI;
 
+/**
+ * Class Service
+ * @package Framework\DI
+ */
 abstract class Service
 {
+    /**
+     * @var array
+     */
     private static $services = array();
 
+    /**
+     * Adds service
+     *
+     * @param string $name
+     * @param $obj
+     */
     public static function set($name, $obj)
     {
         self::$services[$name] = $obj;
     }
 
+    /**
+     * Returns service
+     *
+     * @param string $name
+     * @return mixed
+     */
     public static function get($name)
     {
         return array_key_exists($name, self::$services) ? self::$services[$name] : null;
